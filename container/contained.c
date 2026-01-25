e279e2a24b4d1172ce9046027f4a4700b67e1952
AGachelin <159674898+AGachelin@users.noreply.github.com>
2026-01-25 21:21:23 +0100

    fixed call to clone

--- container/contained.c
+++ container/contained.c
@@ -1,10 +1,11 @@
 #define _GNU_SOURCE
+#include <sched.h>
+#include <sys/syscall.h>
+#include <sys/wait.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sched.h>
-#include <signal.h>
-#include <sys/syscall.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 #define STACK_SIZE 1024 * 64
 // flags for new UTS and PID namespaces (i.e namespace isolation) ; SIGCHLD to notify parent on child termination
@@ -19,19 +20,26 @@ char* create_stack() {
     return stack + STACK_SIZE;
 }
 
-void fn(){
+int fn(void *){
     printf("inside container\n");
+    char *cmd[] = {"/bin/sh", NULL};
+    if(execvp(cmd[0],cmd)){
+        perror("could not execute shell");
+        exit(1);
+    }
 }
 
 int main() {
     char* stack_top = create_stack();
     printf("Stack created at address: %p\n", stack_top);
-    if (syscall(SYS_clone, FLAGS, stack_top, 0, 0, 0) == -1) { // calling clone via syscall
+    pid_t pid = clone(fn, stack_top, FLAGS, 0);
+    if (pid == -1) {
         perror("Failed to create clone");
         free(stack_top - STACK_SIZE);
         exit(1);
     }
     printf("Container created successfully.\n");
+    waitpid(pid, NULL, 0);
     free(stack_top - STACK_SIZE);
     return 0;
 }
