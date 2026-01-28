d2d140b4846bde10a5291fae4d33089d3bee23d7
AGachelin <159674898+AGachelin@users.noreply.github.com>
2026-01-28 03:28:53 +0100

    syscalls blacklist

--- container/container_v2.c
+++ container/container_v2.c
@@ -1,4 +1,5 @@
 // source : https://blog.lizzie.io/linux-containers-in-500-loc.html
+// command to compile : gcc container.c -o container -lseccomp
 #define _GNU_SOURCE
 #include <sched.h>
 #include <grp.h>
@@ -15,6 +16,7 @@
 #include <sys/resource.h>
 #include <sys/stat.h>
 #include <errno.h>
+#include <seccomp.h> // libseccomp-dev should be installed
 
 #define STACK_SIZE 1024 * 1024
 #define USERNS_OFFSET 10000
@@ -25,6 +27,7 @@
 #define PIDS "64"
 #define WEIGHT "10"
 #define FD_COUNT 64
+#define SCMP_FAIL SCMP_ACT_ERRNO(EPERM)
 
 struct child_config {
 	int argc;
@@ -484,13 +487,58 @@ int userns(struct child_config *config)
 	return 0;
 }
 
+// blacklist dangerous syscalls using seccomp
+int syscalls()
+{
+	scmp_filter_ctx ctx = NULL;
+	fprintf(stderr, "=> filtering syscalls...");
+	if (!(ctx = seccomp_init(SCMP_ACT_ALLOW))
+	    || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(chmod), 1,
+				SCMP_A1(SCMP_CMP_MASKED_EQ, S_ISUID, S_ISUID))
+	    || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(chmod), 1,
+				SCMP_A1(SCMP_CMP_MASKED_EQ, S_ISGID, S_ISGID))
+	    || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(fchmod), 1,
+				SCMP_A1(SCMP_CMP_MASKED_EQ, S_ISUID, S_ISUID))
+	    || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(fchmod), 1,
+				SCMP_A1(SCMP_CMP_MASKED_EQ, S_ISGID, S_ISGID))
+	    || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(fchmodat), 1,
+				SCMP_A2(SCMP_CMP_MASKED_EQ, S_ISUID, S_ISUID))
+	    || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(fchmodat), 1,
+				SCMP_A2(SCMP_CMP_MASKED_EQ, S_ISGID, S_ISGID))
+	    || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(unshare), 1,
+				SCMP_A0(SCMP_CMP_MASKED_EQ, CLONE_NEWUSER, CLONE_NEWUSER))
+	    || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(clone), 1,
+				SCMP_A0(SCMP_CMP_MASKED_EQ, CLONE_NEWUSER, CLONE_NEWUSER))
+	    || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(ioctl), 1,
+				SCMP_A1(SCMP_CMP_MASKED_EQ, TIOCSTI, TIOCSTI))
+	    || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(keyctl), 0)
+	    || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(add_key), 0)
+	    || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(request_key), 0)
+	    || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(ptrace), 0)
+	    || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(mbind), 0)
+	    || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(migrate_pages), 0)
+	    || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(move_pages), 0)
+	    || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(set_mempolicy), 0)
+	    || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(userfaultfd), 0)
+	    || seccomp_rule_add(ctx, SCMP_FAIL, SCMP_SYS(perf_event_open), 0)
+	    || seccomp_attr_set(ctx, SCMP_FLTATR_CTL_NNP, 0)
+	    || seccomp_load(ctx)) {
+		if (ctx) seccomp_release(ctx);
+		fprintf(stderr, "failed: %m\n");
+		return 1;
+	}
+	seccomp_release(ctx);
+	fprintf(stderr, "done.\n");
+	return 0;
+}
+
 int child(void *arg){
 	struct child_config *config = arg;
 	if (set_hostname(config->hostname)
 	    || mounts(config)
 	    || userns(config)
 	    //|| capabilities()
-	    //|| syscalls()
+	    || syscalls()
         ) {
 		close(config->fd);
 		return -1;
